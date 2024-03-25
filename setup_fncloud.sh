ee7ae5eeb6597c0908d762929e0eb7d74d51f4ef
Isaac Fernandes <101341132+Eyezuhk@users.noreply.github.com>
2024-03-25 10:18:15 -0300

    Update setup_fncloud.sh

--- setup_fncloud.sh
+++ setup_fncloud.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+echo "..................::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::......................
+...................:::::::::::::::::::::::::::~!7???7!^::::::::::::::::::::::.......................
+......................::::::::::::::::::::~YB&@@@@@@@@@&BJ~:::::::::::::::::........................
+.......................:::::::::::::::::7B@@@@@@@@@@@@@@@@@B!::::::::::::...........................
+..........................::::::::::::^G@@@@@@@@@@@@@@@@@@@@@P:.:::..::.............................
+..............................::......5#&&&&&&#BPYYYYPB#&&&&&@B:....................................
+.................................:~7?J?7!!YB5!!?YPPP5Y7!!5#&&&&Y....................................
+...............................!G&&&&&&&&G7::P#&&&&&&&&#P~^JP#&G:...................................
+..............................?####&&&&##&&P:JBY?7777?YG#B57^^5P....................................
+.............................^7777!!75####B#7.^7YPPPPY7^~5BGP!.^^7J555J7~:..........................
+..........................~5B######GY~^YBBBY.7GBGYJJJYY?:.7JJ7:~Y5PPPPPP5J^.........................
+........................:5######BBBBBBY.!5?..77?:~?????????????JJJJJJJYYYY5Y^.......................
+........................P##BBBBBBBGGGGY::~7JJJ?7~^:^!!!!!77777777777777?????!.......................
+.......................!#BBBGGGGGGGG5^:75555J????7:.~???????????????JJJJJJJJJ^......................
+.......................!BGGGGGPPPPPJ.:Y5YYJ:^7777!!~^..~!77!!7777777777777777:......................
+.......................:PGPPPPP555Y~ 7YYJJ?~^~^^^~~~^::^~~~~~~~~~~~~~~~~!!!!~.......................
+........................^PPP5555YYJ~ ~YJJ!!!!^:.:!!!!!!!!!!!!!!!!!!!!777!777:.......................
+........................ .7555YYYJJ7:.~?J?~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^..........................
+.......................... .~7JJJJJ?7~:::^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^::...........................
+............................  ..:::::::....                             ............................
+.................................    ...............................................................
+............................... ................. .... .............................................
+.....................5BGGGG: ~#Y. .YB...:#?......?GBBGJ....7GBBG!...:BB....~#~......................
+.....................&@!!!:. !@@&?.B@:..^@P ....&&!::!&&..#@7::!^...&@@#.. ?@7 .....................
+.....................#@PP5.. !@J!&&&@:..:@5 ...:@#.  .B@:.@&.  ....#@PG@B. ?@7 .....................
+.....................#&.  .. !@7 .J@@:..:@&GGB! ~B#GG#B~..^B&BB#J 5@J77J@Y 7@#GBY...................
+......................:.......:... .:. ..:::::..  .:^:  ... .::...:.    ::..:::::...................
+.....................................^^....:.....^^...^.:....^^:....................................
+................................... 75!:...G^...5??J .P^5^...G7P:...................................
+.....................................^^....^~:...^^...:~^....^^:....................................
+.....................................  ..........  .... .....  ....................................."
+
 # Function to check if the user is root
 is_root() {
     if [ "$(id -u)" -ne 0 ]; then
@@ -8,6 +40,21 @@ is_root() {
     fi
 }
 
+# Cleanup function
+cleanup() {
+    echo "Cleaning up..."
+    iptables -D INPUT -m state --state NEW -p tcp --dport "$AGENT_PORT" -j ACCEPT 2>/dev/null
+    iptables -D INPUT -m state --state NEW -p tcp --dport "$CLIENT_PORT" -j ACCEPT 2>/dev/null
+    netfilter-persistent save >/dev/null 2>&1
+    systemctl stop fncloud.service >/dev/null 2>&1
+    systemctl disable fncloud.service >/dev/null 2>&1
+    rm -rf /usr/local/bin/FNCloud.py /var/log/fncloud /etc/systemd/system/fncloud.service >/dev/null 2>&1
+    echo "Cleanup complete."
+}
+
+# Trap the exit signal and call the cleanup function
+trap cleanup EXIT
+
 # Check if the user is root
 is_root
 
@@ -15,8 +62,18 @@ is_root
 install_python() {
     if ! command -v python3 >/dev/null 2>&1; then
         echo "Python3 is not installed. Installing Python3..."
-        apt-get update
-        apt-get install -y python3
+        if [ -x "$(command -v apt-get)" ]; then
+            apt-get update
+            apt-get install -y python3
+        elif [ -x "$(command -v yum)" ]; then
+            yum install -y python3
+        elif [ -x "$(command -v zypper)" ]; then
+            zypper install -y python3
+        else
+            echo "Could not find an appropriate package manager to install Python3."
+            echo "Please install Python3 manually and rerun the script."
+            exit 1
+        fi
         if ! command -v python3 >/dev/null 2>&1; then
             echo "Failed to install Python3. Please install it manually and rerun the script."
             exit 1
@@ -47,8 +104,19 @@ check_dependencies() {
         for dep in "${missing_deps[@]}"; do
             echo "- $dep"
         done
-        echo "Please install the missing dependencies before continuing."
-        exit 1
+        echo "Installing missing dependencies..."
+        if [ -x "$(command -v apt-get)" ]; then
+            apt-get update
+            apt-get install -y "${missing_deps[@]}"
+        elif [ -x "$(command -v yum)" ]; then
+            yum install -y "${missing_deps[@]}"
+        elif [ -x "$(command -v zypper)" ]; then
+            zypper install -y "${missing_deps[@]}"
+        else
+            echo "Could not find an appropriate package manager to install dependencies."
+            echo "Please install the missing dependencies manually before continuing."
+            exit 1
+        fi
     fi
 }
 
@@ -90,10 +158,13 @@ mv FNCloud.py /usr/local/bin/ || { echo "Failed to move the FNCloud.py file to /
 mkdir -p /var/log/fncloud || { echo "Failed to create the /var/log/fncloud directory."; exit 1; }
 
 # Set the correct permissions for the program
-chmod 755 /usr/local/bin/FNCloud.py || { echo "Failed to set permissions for the FNCloud.py file."; exit 1; }
+chmod 640 /usr/local/bin/FNCloud.py || { echo "Failed to set permissions for the FNCloud.py file."; exit 1; }
+
+# Get the current user
+current_user=$(whoami)
 
 # Set the correct permissions for the log directory
-chown -R ubuntu:ubuntu /var/log/fncloud || { echo "Failed to set permissions for the /var/log/fncloud directory."; exit 1; }
+chown -R "$current_user:$current_user" /var/log/fncloud || { echo "Failed to set permissions for the /var/log/fncloud directory."; exit 1; }
 
 # Configure firewall rules
 echo "Configuring firewall rules..."
@@ -126,10 +197,13 @@ Description=FNCloud
 After=network.target
 
 [Service]
-User=ubuntu
-Environment=HOME=/home/ubuntu
+User=$current_user
+Environment=HOME=/home/$current_user
 ExecStart=/usr/bin/sudo /usr/bin/python3 /usr/local/bin/FNCloud.py > /var/log/fncloud/output_server.log 2>&1
 Restart=always
+CapabilityBoundingSet=CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
 
 [Install]
 WantedBy=multi-user.target
